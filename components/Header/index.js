eabb14188de665257bfb2b81605eeebb074c396b
刘伟波 <wb-lwb414922@antfin.com>
2018-11-05 16:20:19 +0800

    style:mobile 導航64px

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,22 +1,22 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {Layout, Menu, Breadcrumb, Row, Col, message} from 'antd';
-import Link from 'next/link'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Layout, Menu, Breadcrumb, Row, Col, message } from 'antd';
+import Link from 'next/link';
 // prefetch预加载
 import dynamic from 'next/dynamic';
 import * as ROUTER from '../../config/constantsData';
-import {getBlogUrl, postAdminPasswordUrl, getTotalUrl} from '../../config';
-import {getSearchList, getSearchTotal, postAdminPassword, collectArticleList} from '../../store/actions';
+import { getBlogUrl, postAdminPasswordUrl, getTotalUrl } from '../../config';
+import { getSearchList, getSearchTotal, postAdminPassword, collectArticleList } from '../../store/actions';
 import MyHead from '../../components/MyHead';
-import {MENU_RULE, pageNum} from "../../config/constantsData";
-import './index.less'
-import {VERSIONS_TXT} from "../../config/constantsData";
+import { MENU_RULE, pageNum } from '../../config/constantsData';
+import './index.less';
+import { VERSIONS_TXT } from '../../config/constantsData';
 
 //toptis不需要seo
 const DynasicTopTipsNoSsr = dynamic(import('../../components/TopTips'), {
   ssr: false
-})
-const {Header, Content} = Layout;
+});
+const { Header, Content } = Layout;
 const routes = [
   {
     href: ROUTER.INDEX,
@@ -71,7 +71,7 @@ const routes = [
     isSuperAdmin: true,
     exit: true
   },
-]
+];
 
 class TopNav extends Component {
   constructor(props) {
@@ -80,56 +80,57 @@ class TopNav extends Component {
       isLogin: false,
       isAll: false,
       defaultSelectedKeys: '/'
-    }
+    };
   }
 
   componentDidMount() {
-    const {pathname = '/'} = location;
-    const {dispatch} = this.props;
-    const {password = ''} = sessionStorage;
+    const { pathname = '/' } = location;
+    const { dispatch } = this.props;
+    const { password = '' } = sessionStorage;
     this.setState({
-      selectedKeys: MENU_RULE[pathname]||MENU_RULE['/']
-    })
+      selectedKeys: MENU_RULE[pathname] || MENU_RULE['/']
+    });
     if (!password) {
       return;
     }
-    postAdminPassword(dispatch, postAdminPasswordUrl(), {password: password}).then(res => {
-      const {postAdminPasswordData = []} = res;
-      if (postAdminPasswordData.length) {
-        this.setState({
-          isLogin: true
-        })
-      }
-    })
+    postAdminPassword(dispatch, postAdminPasswordUrl(), { password: password })
+      .then(res => {
+        const { postAdminPasswordData = [] } = res;
+        if (postAdminPasswordData.length) {
+          this.setState({
+            isLogin: true
+          });
+        }
+      });
   }
 
-  onSelect({keyPath, key}) {
+  onSelect({ keyPath, key }) {
     this.setState({
       selectedKeys: keyPath
-    })
+    });
   }
 
   onExit() {
     //退出清除sessionStorage
-    sessionStorage.removeItem('password')
+    sessionStorage.removeItem('password');
     this.setState({
       isLogin: false
-    })
+    });
   }
 
   onMyCollect(articleList) {
-    const {isAll} = this.state;
+    const { isAll } = this.state;
     let bool = articleList === 'articleList';
-    const {dispatch} = this.props;
+    const { dispatch } = this.props;
     !bool && this.setState({
       isAll: !isAll
-    })
+    });
     let collectArrStr = localStorage.getItem('collectArrStr');
     let collectArr;
     try {
-      collectArr = JSON.parse(collectArrStr)
+      collectArr = JSON.parse(collectArrStr);
     } catch (err) {
-      collectArr = []
+      collectArr = [];
     }
     if (!Array.isArray(collectArr) || !collectArr.length) {
       message.warning(`还没有收藏过文章，点击全部文章，然后点击小星星进行本地收藏！`);
@@ -142,34 +143,34 @@ class TopNav extends Component {
         num: 1,
         pageNum,
         wd: ''
-      }
+      };
       let queryTotalString = {
         type: 'all',
         wd: ''
       };
-      getSearchList(dispatch, getBlogUrl(queryStringObj))
-      getSearchTotal(dispatch, getTotalUrl(queryTotalString))
+      getSearchList(dispatch, getBlogUrl(queryStringObj));
+      getSearchTotal(dispatch, getTotalUrl(queryTotalString));
       if (bool) {
-        collectArticleList(dispatch, false)
+        collectArticleList(dispatch, false);
         return;
       }
     } else {
-      getSearchList(dispatch, 'myCollect', collectArr)
+      getSearchList(dispatch, 'myCollect', collectArr);
     }
-    collectArticleList(dispatch, !this.state.isAll)
+    collectArticleList(dispatch, !this.state.isAll);
   }
 
   onMyFn(item) {
-    const {isCollectArticle} = this.props;
+    const { isCollectArticle } = this.props;
     let fn = () => {
     };
     if (item.exit) {
-      fn = this.onExit.bind(this)
+      fn = this.onExit.bind(this);
     } else if (item.isMyCollcet) {
-      fn = this.onMyCollect.bind(this)
+      fn = this.onMyCollect.bind(this);
     }
-    else if (item.isArticleList&&isCollectArticle) {
-      fn = this.onMyCollect.bind(this, 'articleList')
+    else if (item.isArticleList && isCollectArticle) {
+      fn = this.onMyCollect.bind(this, 'articleList');
     }
     return <Menu.Item key={item.txt}>
       <Link prefetch href={item.href}>
@@ -179,24 +180,24 @@ class TopNav extends Component {
   }
 
   render() {
-    let {isLogin, selectedKeys = ['/']} = this.state;
-    const {userAgent = 'pc'} = this.props;
+    let { isLogin, selectedKeys = ['/'] } = this.state;
+    const { userAgent = 'pc' } = this.props;
     if (Object.prototype.toString.call(selectedKeys) === '[object String]') {
-      selectedKeys = ['/']
+      selectedKeys = ['/'];
     }
     const regSelectKey = /\/blog[\/]?[0-9]?/;
     if (selectedKeys[0] && regSelectKey.test(selectedKeys[0])) {
-      selectedKeys = ['/blog']
+      selectedKeys = ['/blog'];
     }
     let newRoutes;
     if (isLogin) {
-      newRoutes = routes
+      newRoutes = routes;
     } else {
-      newRoutes = routes.filter(v => !v.isSuperAdmin)
+      newRoutes = routes.filter(v => !v.isSuperAdmin);
     }
     return (
       <div>
-        <MyHead/>
+        <MyHead />
         <Layout>
           <Header style={{
             position: userAgent === 'pc' ? 'fixed' : 'static',
@@ -207,20 +208,20 @@ class TopNav extends Component {
           }}>
             <Row>
               <Col span={2}></Col>
-              <Col sm={{span: 24}}
-                   xs={{span: 24}}
-                   lg={{span: 17}}>
+              <Col sm={{ span: 24 }}
+                   xs={{ span: 24 }}
+                   lg={{ span: 17 }}>
                 <Menu
                   theme="dark"
                   mode="horizontal"
                   onClick={this.onSelect.bind(this)}
                   defaultSelectedKeys={['/']}
                   selectedKeys={selectedKeys}
-                  style={{lineHeight: '64px'}}
+                  style={{ lineHeight: '64px' }}
                 >
                   {
                     newRoutes.map((item, index) => {
-                      return this.onMyFn(item)
+                      return this.onMyFn(item);
 
                     })
                   }
@@ -230,12 +231,12 @@ class TopNav extends Component {
           </Header>
 
           <Row>
-            <Col sm={{span: 24, offset: 0}}
-                 xs={{span: 24, offset: 0}}
-                 lg={{span: 20, offset: 2}}>
+            <Col sm={{ span: 24, offset: 0 }}
+                 xs={{ span: 24, offset: 0 }}
+                 lg={{ span: 20, offset: 2 }}>
 
-              <Content style={{marginTop: 64}}>
-                <DynasicTopTipsNoSsr/>
+              <Content style={{ marginTop: userAgent === 'pc' ? 64 : 0 }}>
+                <DynasicTopTipsNoSsr />
               </Content>
             </Col>
           </Row>
@@ -246,8 +247,8 @@ class TopNav extends Component {
 }
 
 const mapStateToProps = state => {
-  const {postAdminPasswordData,isCollectArticle} = state
-  return {postAdminPasswordData,isCollectArticle};
-}
-export default connect(mapStateToProps)(TopNav)
+  const { postAdminPasswordData, isCollectArticle } = state;
+  return { postAdminPasswordData, isCollectArticle };
+};
+export default connect(mapStateToProps)(TopNav);
 
